2d124723745283551df2af89b13a0465e80883a8
Fred Brennan <copypaste@kittens.ph>
2022-10-23 10:43:39 -0400

    Performance upgrades

--- playsong.sh
+++ playsong.sh
@@ -3,19 +3,19 @@ INPUT="$1"
 COVER="$(dirname -- "$1")/cover.jpg"
 SUBS="$2"
 OUT="$3"
-OUTRATE="${OUTRATE:-60}"
+OUTRATE="${OUTRATE:-30}"
 OUTQP="${OUTQP:-23}"
 OUTHEIGHT="${OUTHEIGHT:-1080}"
 OUTWIDTH="${OUTWIDTH:-1920}"
 if [ ! -z "$SUBS" ]; then
-    SUBS_ADD="[o]; [o]subtitles=$SUBS,"
+    SUBS_ADD="subtitles=$SUBS,"
 else
-    SUBS_ADD="[o]; [o]"
+    SUBS_ADD=""
 fi
-if [ ! -z "$HEVC" ]; then
-    VCODEC=hevc_nvenc
-else
-    VCODEC=h264_nvenc
+if [ ! -z "$HEVC" -a -z "$VCODEC" ]; then
+    VCODEC="hevc_nvenc -qp $OUTQP"
+elif [ -z "$VCODEC" ]; then
+    VCODEC="h264_nvenc -qp $OUTQP"
 fi
 if [ ! -z "$FLAC" ]; then
     ACODEC=copy
@@ -23,7 +23,8 @@ else
     ACODEC='aac -b:a 128k'
 fi
 
-OUTHEIGHTEQ=$(echo "($OUTHEIGHT/2)-($OUTHEIGHT/10)" | bc)
+OUTHEIGHTEQ=$(bc <<< "($OUTHEIGHT/2)-($OUTHEIGHT/10)")
+OVERLAYEQ=$(bc <<< "$OUTHEIGHT-$OUTHEIGHTEQ")
 
 export __NV_PRIME_RENDER_OFFLOAD=1
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
@@ -35,22 +36,25 @@ else
 fi
 mapfile -d '' COMMAND < <(head -c -1 << 'EOF'
 ffmpeg -i "$INPUT" -hwaccel cuda -hwaccel_output_format cuda -i "$COVER" \
--filter_complex "showspectrum=slide=scroll : color=intensity : mode=separate,
-format=rgba, colorchannelmixer=1:0:0:0:0:1:0:0:0:0:1:0:1:1:1:0, setsar=1,
-hwupload_cuda, scale_npp=$OUTWIDTH : $OUTHEIGHTEQ[t]; [1:v]scale_npp=$OUTWIDTH
-: $OUTHEIGHT : force_original_aspect_ratio=decrease : format=yuv420p,
-hwdownload, pad=$OUTWIDTH : $OUTHEIGHT : 0 : (oh-ih)/2,
-loop=-1:1:1${SUBS_ADD}hwupload_cuda[bg];
-[bg][t]overlay_cuda=y=$OUTHEIGHT-($OUTHEIGHTEQ) : shortest=1" -c:v $VCODEC -qp \
-"${OUTQP}" -frame_rate 30 -c:a $ACODEC
+-filter_complex \
+    "showspectrum=slide=scroll : color=intensity : mode=separate, format=rgba,
+    colorchannelmixer=1:0:0:0:0:1:0:0:0:0:1:0:1:1:1:0, setsar=1, hwupload_cuda,
+    scale_npp=$OUTWIDTH : $OUTHEIGHTEQ[t];
+
+    [1:v]scale_npp=$OUTWIDTH : $OUTHEIGHT : force_original_aspect_ratio=1 :
+    format=yuv420p, hwdownload, pad=$OUTWIDTH : $OUTHEIGHT : 0 : (oh-ih)/2,
+    loop=-1:1:1, ${SUBS_ADD} hwupload_cuda [bg];
+
+    [bg][t]overlay_cuda=y=$OVERLAYEQ: shortest=1, hwdownload, format=yuv420p" \
+-frame_rate "$OUTRATE" -c:v $VCODEC -c:a $ACODEC
 EOF
 )
-PLAYER=$(hash mpv && echo mpv || echo ffplay)
+PLAYER=$(hash mpv && which mpv || which ffplay)
 set -x
 if [ $WRAP -eq 0 ]; then
-    eval "$COMMAND $OUT"
+    eval "$COMMAND" "$(printf "%q" "$OUT")"
 else
-    $PLAYER <(eval "$COMMAND $OUT")
-    reset
+    "$PLAYER" <(eval "$COMMAND" $OUT)
 fi
 set +x
+stty sane
